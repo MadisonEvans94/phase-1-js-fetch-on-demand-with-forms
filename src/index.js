e1aa0a414e377f9f3d833d3907d5c4d2cad455e5
Madison Evans <m.corbinevans@gmail.com>
2023-01-17 06:58:21 -0700

    final code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+	let tempVal;
+	const inputForm = document.querySelector("form");
+	inputForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		console.log(document.querySelector("#searchByID").value);
+		tempVal = document.querySelector("#searchByID").value;
+		fetch(`http://localhost:3000/movies/${tempVal}`)
+			.then((response) => response.json())
+			.then((data) => {
+				const title = document.querySelector("section#movieDetails h4");
+				const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+				title.innerText = data.title;
+				summary.innerText = data.summary;
+			});
+	});
+};
+
+document.addEventListener("DOMContentLoaded", init);
